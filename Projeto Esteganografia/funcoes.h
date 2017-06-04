51866cb29d46cb448ccaed978166b65d3677e537
JoMedeiros <josivan.medeiros@yahoo.com.br>
2017-06-03 22:19:25 -0300

    Inicio da modularização
    Está faltando:
    -Verificar se a mensagem é maior do que a imagem pode suportar
    -Separar Cifrar.sh e Decifrar.sh dentro do arquivo (através de getopt)
    -Criar mais funções

---
+++ Projeto Esteganografia/funcoes.h
@@ -0,0 +1,25 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+enum colors {
+	red=0, green, blue, alpha
+};
+typedef struct
+{
+	unsigned char rgb[3];
+}PixelRGB;
+
+void getHeader (FILE *image, char *formato, int* xSize, int* ySize, int* range);
+/* Alocação e desalocação da Matriz */
+void alocarMatrizDePixel (PixelRGB*** pixelMatrix, int xSize, int ySize);
+void desalocarMatrizDePixel (PixelRGB*** pixelMatrix, int ySize);
+/* Coloca os caracteres na imagem */
+PixelRGB** swapLastBit (PixelRGB **pixel, char characater);
+/* Recupera os caracteres da imagem */
+void getMessage (FILE *image, char* word, int wordSize);
+
+#endif
