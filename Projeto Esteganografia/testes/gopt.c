cd94295471790ef424925e16671347ba36bbec40
JoMedeiros <josivan.medeiros@yahoo.com.br>
2017-06-08 23:20:46 -0300

    getopt concluído

--- Projeto Esteganografia/testes/gopt.c
+++ Projeto Esteganografia/testes/gopt.c
@@ -10,7 +10,9 @@ int main (int argc, char **argv)
 	http://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html#Example-of-Getopt */
 	int eflag = 0;
 	int dflag = 0;
+	int sflag = 0;
 	char *input_file = NULL;
+	char *output_file = NULL;
 	char *format = NULL;
 	char *image = NULL;
 	int index;
@@ -18,13 +20,30 @@ int main (int argc, char **argv)
 
 	opterr = 0;
 
-	while ((i = getopt (argc, argv, "edif:")) != -1){
+	while ((i = getopt (argc, argv, "edi:f:o:s")) != -1){
 		switch (i)
 			{
-			case 'e': eflag = 1; break;
-			case 'd': dflag = 1; break;
-			case 'i': input_file = optarg; break;
-			case 'f': format = optarg; image = optarg; break;
+			case 'e': 
+				if (dflag !=0) fprintf(stderr, "-e option is not allowed along with -c option.\n");
+				else eflag = 1; 
+				break;
+			case 'd': 
+				if (dflag !=0) fprintf(stderr, "-d option is not allowed along with -e option.\n");
+				else dflag = 1; 
+				break;
+			case 'i': 
+				input_file = optarg; 
+				break;
+			case 'f': 
+				format = optarg;
+				break;
+			case 'o':
+				if (dflag !=0) fprintf(stderr, "-o option is not allowed along with -s option.\n");
+				else output_file = optarg;
+				break;
+			case 's':
+				sflag = 1;
+				break;
 			case '?':{
 				if (optopt == 'i')
 					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
@@ -39,10 +58,10 @@ int main (int argc, char **argv)
 				abort ();
 		}
 	}
-	printf ("aflag = %d, bflag = %d, input_file = %s\n", eflag, dflag, input_file);
-	printf("formato = %s, imagem: %s\n", image, input_file);
-
+	if (optind < argc)
+		image = argv[optind++]; // pega a imagem como último argumento depois das opções
 	for (index = optind; index < argc; index++)
 		printf ("Non-option argument %s\n", argv[index]);
+	
 	return 0;
 }
